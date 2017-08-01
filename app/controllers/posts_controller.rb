ec5f1ea2eb2bda583dc78d33616110d3ba195873
dominiccodes <email@domcodes.com>
2017-08-01 16:56:31 +0100

    created form partial for edit and show. index now shows list of posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
 	
 	def show
@@ -16,6 +17,19 @@ class PostsController < ApplicationController
 			redirect_to @post
 		else
 			render "new"
+		end
+	end
+	
+	def edit
+		@post = Post.find(params[:id])
+	end
+	
+	def update
+		@post = Post.find(params[:id])
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render "edit"
 		end
 	end
 	
