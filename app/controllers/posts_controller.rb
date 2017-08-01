150bd45dd0a320f9a61b4a12ab628ecbc352eb14
dominiccodes <email@domcodes.com>
2017-08-01 17:08:07 +0100

    added find post before action for DRY code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	
 	def index
 		@posts = Post.all.order("created_at DESC")
 	end
 	
 	def show
-		@post = Post.find(params[:id])
 	end
 	
 	def new
@@ -21,11 +22,9 @@ class PostsController < ApplicationController
 	end
 	
 	def edit
-		@post = Post.find(params[:id])
 	end
 	
 	def update
-		@post = Post.find(params[:id])
 		if @post.update(post_params)
 			redirect_to @post
 		else
@@ -33,9 +32,16 @@ class PostsController < ApplicationController
 		end
 	end
 	
+	def destroy
+	end
+	
 	private
 	
 	def post_params
 		params.require(:post).permit(:title, :body)
+	end
+	
+	def find_post
+				@post = Post.find(params[:id])
 	end
 end
